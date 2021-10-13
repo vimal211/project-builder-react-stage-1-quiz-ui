679d8385b8bbe41208598fb414088eda11cacea6
Vimal M <vimalnavee2110@gmail.com>
2021-10-13 11:19:25 +0530

    Progression 1

---
+++ quiz/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./Home.css"
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="home_Container">
+                <h3>Quiz App</h3>
+                <button>Play</button>
+            </div>
+        )
+    }
+}
